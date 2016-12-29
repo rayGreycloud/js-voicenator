b4372b0d9fd21ed5c0139c5eea4be6151fc24ace
rayGreycloud <rayGreycloud@gmail.com>
2016-12-29 10:56:32 -0800

    Add event handler to set options when changed

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
 // Grab text to voice
 msg.text = document.querySelector('[name="text"]').value;
+
 // Create voice selection dropdown
 function populateVoices() {
   voices = this.getVoices();
@@ -19,11 +20,35 @@ function populateVoices() {
     .join('');
   voicesDropdown.innerHTML = voiceOptions;
 }
+
 // Find selected voice and set msg.voice
 function setVoice() {
   console.log('Changing voice...');
   msg.voice = voices.find(voice => voice.name === this.value);
+  // Stop and restart
+  toggle();
 }
 
+// Restart upon change in voice, options or text
+function toggle(startOver = true) {
+  // Cancel current speak
+  speechSynthesis.cancel();
+  if (startOver) {
+  // Restart
+  speechSynthesis.speak(msg);
+  }
+}
+
+// Change options and restart
+function setOption() {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
+
+// Listen for changes in available voices
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
+// Listen for change in selected voice
 voicesDropdown.addEventListener('change', setVoice);
+// Listen for change in options
+options.forEach(option => option.addEventListener('change', setOption));
